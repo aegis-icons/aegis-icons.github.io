584eefff0f10177456d0cf741bc43762e796ce1f
Kristian Salonen <github@krisu.anonaddy.com>
2022-01-31 02:21:22 +0200

    Gem things

--- start_jekyll_server.bat
+++ start_jekyll_server.bat
@@ -1,8 +1,7 @@
 @echo off
-echo|set /p=http://127.0.0.1:4000/|clip
 echo.
-echo Jekyll server address copied to your clipboard:
-echo http://127.0.0.1:4000/
+echo Jekyll server address:
+echo http://localhost:4000/
 echo.
 echo ----
 echo.
