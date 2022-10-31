308c1f42d7be7a0279b46acf17f4471dfb9ca35d
Kristian Salonen <github@krisu.anonaddy.com>
2022-10-31 17:59:10 +0200

    Update get_icons.bat

--- get_icons.bat
+++ get_icons.bat
@@ -1,8 +1,8 @@
 set website-root=%cd%
 cd ..
 cd aegis-icons
-cd SVG
-robocopy "%cd%" "%website-root%\icons\primary" /mir /xd .Generic .Outdated .Variations
-robocopy "%cd%\.Generic" "%website-root%\icons\generic" /mir
-robocopy "%cd%\.Outdated" "%website-root%\icons\outdated" /mir
-robocopy "%cd%\.Variations" "%website-root%\icons\variations" /mir
+cd icons
+robocopy "%cd%\1_Primary" "%website-root%\icons\primary" /mir
+robocopy "%cd%\2_Variations" "%website-root%\icons\variations" /mir
+robocopy "%cd%\3_Generic" "%website-root%\icons\generic" /mir
+robocopy "%cd%\4_Outdated" "%website-root%\icons\outdated" /mir
