c25d2ce10d66122836b8a96e0bf1aff968c33109
Kristian Salonen <3540275+krisu5@users.noreply.github.com>
2026-04-02 14:32:54 +0300

    Update 2__start_jekyll_server.sh

--- z__sh-scripts/2__start_jekyll_server.sh
+++ z__sh-scripts/2__start_jekyll_server.sh
@@ -3,4 +3,4 @@ echo -ne "\033]0;Local Jekyll server for aegis-icons website\007"
 cd ..
 echo -e "\033[0;31mDon't close the window, use CTRL+C instead! Otherwise Ruby stays running in the background\033[0m"
 echo
-jekyll serve --host localhost --open-url
+jekyll serve --incremental --host 0.0.0.0 --port 4000 --open_url
