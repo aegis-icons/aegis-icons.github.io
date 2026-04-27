5aa714dbc9fb0eed591d45182205117e67a45ca5
Kristian Salonen <3540275+krisu5@users.noreply.github.com>
2026-04-27 17:08:34 +0300

    Disclaimer updates

--- z__bat-scripts/2__start_jekyll_server.bat
+++ z__bat-scripts/2__start_jekyll_server.bat
@@ -1,7 +1,7 @@
 @echo off
 title Local Jekyll server for aegis-icons website
 cd ..
-echo [41mDon't close the window, use CTRL+C instead! Otherwise Ruby stays running in the background![0m
+echo [41m Don't close the window, use CTRL+C instead! Otherwise Ruby stays running in the background! [0m
 echo.
 bundle exec jekyll serve --incremental --host 0.0.0.0 --port 4000 --open_url
 pause
